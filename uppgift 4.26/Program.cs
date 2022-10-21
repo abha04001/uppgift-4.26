c0c9b907ac7a92206c5bfba08a137e436a0f1fa8
caspian <casa04001@utb.vaxjo.se>
2022-10-21 14:12:18 +0200

    Add project files.

---
+++ uppgift 4.26/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Uppgift_4._22
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+            
+
+
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra != 9)
+                    Console.WriteLine(siffra + 1);
+                else if (siffra == 9) ;   
+                    Console.WriteLine(siffra + 0);
+            }
+        }
+    }
+}
